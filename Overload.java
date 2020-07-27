665ffbed630a97f6a03a84898ca061185f924832
nehakumari0201 <47860946+nehakumari0201@users.noreply.github.com>
2020-07-26 23:02:25 -0800

    Add files via upload

--- Overload.java
+++ Overload.java
@@ -1,28 +1,28 @@
 class OverloadDemo{
 	void test() {
-		System.out.println("No parameters");
+		System.out.println("No parameter");
 	}
 	void test(int a) {
-		System.out.println("a: "+a);
+		System.out.println("a : "+a);
 	}
-	void test(int a,int b) {
-		System.out.println("a and b: " +a+" "+b);
+	void test(int a, int b) {
+		System.out.println("a and b "+a+" "+b);
 	}
 	double test(double a) {
-		System.out.println("double a:" +a);
+		System.out.println("double a: "+a);
 		return a*a;
 	}
 }
-public class Overload {
+ class Overload {
 
 	public static void main(String[] args) {
-		 OverloadDemo ob= new OverloadDemo();
-	     double result;
-		 ob.test();
-		 ob.test(10);
-		 ob.test(10,20);
-		result=   ob.test(123.25);
-		System.out.println("Resultof  ob.test(123.25)"+result);
+		OverloadDemo ob=new OverloadDemo();
+		double result;
+		ob.test();
+		ob.test(10);
+		ob.test(10,20);
+		 result=ob.test(123.25);
+		System.out.println("Resultof the tes(123.25):" +result);
 
 	}
 
--- OverloadCons.java
+++ OverloadCons.java
@@ -1,21 +1,25 @@
-class Box{
+-class Box {
 	double width;
 	double height;
 	double depth;
-	Box(double w,double h,double d){
-		width =w;
+	Box(double w,double h,double d) 
+	{
+		width=w;
 		height=h;
 		depth=d;
 	}
-	Box(){
+	Box()
+	{
 		width=-1;
-		height =-1;
+		height=-1;
 		depth=-1;
 	}
-	Box(double len){
+	Box(double len)
+	{
 		width=height=depth=len;
 	}
-	double volume() {
+	double volume()
+	{
 		return width*height*depth;
 	}
 }
@@ -24,16 +28,13 @@ public class OverloadCons {
 	public static void main(String[] args) {
 		 Box mybox1=new Box(10,20,15);
 		 Box mybox2=new Box();
-		 Box mycube =new Box(7);
+		 Box mybox3=new Box(7);
 		 double vol;
 		 vol=mybox1.volume();
-		 System.out.println("Volume of mybox1 is "+vol);
+		 System.out.println("volume of mybox is " +vol);
 		 vol=mybox2.volume();
-		 System.out.println("volume of mybox2 is "+vol);
-		 vol=mycube.volume();
-		 System.out.println("volume of mycube is "+vol);
-		 
-
+		 System.out.println("Volume of mybox2 is "+vol);
+		 vol=mybox3.volume();
+		 System.out.println("Volume of mybox3 is "+vol);
 	}
-
 }
